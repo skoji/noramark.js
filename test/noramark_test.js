38f898f4549b272a3323bc5693317667999fa3c7
Satoshi KOJIMA <skoji@mac.com>
2014-04-24 00:11:12 +0900

    add html generator (but does nothing for the present)

--- test/noramark_test.js
+++ test/noramark_test.js
@@ -7,7 +7,8 @@ var xmlHelper = require('./xml_helper.js');
 describe('NoraParser', function() {
   it('converts simple paragraph', function () {
     var text = "ここから、パラグラフがはじまります。\n「二行目です。」\n三行目です。\n\n\nここから、次のパラグラフです。";
-    var converted = noraParser.parse(text);
+    noraParser.parse(text);
+    var converted = noraParser.generators.html();
     var $ = cheerio.load(converted);
     expect($('body').length).to.eq(1);
     expect(xmlHelper.toA($('body')[0])).to.eql(
